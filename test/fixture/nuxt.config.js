8201cb69d93511562c9ca9e8260fbe457ae2a15f
Gomah <hi@gomah.fr>
2020-09-21 22:44:30 +1000

    test(): Fix fakeql endpoint

--- test/fixture/nuxt.config.js
+++ test/fixture/nuxt.config.js
@@ -5,7 +5,7 @@ module.exports = {
   srcDir: __dirname,
   modules: ['../../lib/module'],
   graphql: {
-    endpoint: 'https://fakeql.com/graphql/e893c14bbdb41e38063e5aab5c67f7de',
+    endpoint: 'https://fakeql.com/graphql/e47f9af8a27e886dc2573ae87cfa1de6',
     options: {},
   },
   dev: process.env.NODE_ENV !== 'test' && process.env.NODE_ENV === 'production',
