4c1d305d7ac992cf68499a41e4bcb933762ebfb8
Gomah <hi@gomah.fr>
2021-02-22 18:34:35 +1100

    types(): Adjust types for v4

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,52 +1,48 @@
 import Vue from 'vue';
 import './vuex';
 import type { GraphQLClient } from 'graphql-request';
-import type { Variables, GraphQLError } from 'graphql-request/dist/types';
-import type { ASTNode } from 'graphql';
-
-interface NuxtGraphQLInstance extends GraphQLClient {
-  rawRequest<T extends any>(
-    query: string | ASTNode,
-    variables?: Variables
-  ): Promise<{
-    data?: T;
-    extensions?: any;
-    headers: Headers;
-    status: number;
-    errors?: GraphQLError[];
-  }>;
-  request<T extends any>(query: string | ASTNode, variables?: Variables): Promise<T>;
-}
 
 declare module '@nuxt/vue-app' {
   interface Context {
-    $graphql: NuxtGraphQLInstance;
+    $graphql: {
+      [client: string]: GraphQLClient;
+    };
   }
 
   interface NuxtAppOptions {
-    $graphql: NuxtGraphQLInstance;
+   $graphql: {
+      [client: string]: GraphQLClient;
+    };
   }
 }
 
 // Nuxt 2.9+
 declare module '@nuxt/types' {
   interface Context {
-    $graphql: NuxtGraphQLInstance;
+   $graphql: {
+      [client: string]: GraphQLClient;
+    };
   }
 
   interface NuxtAppOptions {
-    $graphql: NuxtGraphQLInstance;
+   $graphql: {
+      [client: string]: GraphQLClient;
+    };
   }
 }
 
 declare module 'vue/types/vue' {
   interface Vue {
-    $graphql: NuxtGraphQLInstance;
+   $graphql: {
+      [client: string]: GraphQLClient;
+    };
   }
 }
 
 declare module 'vuex' {
   interface Store<S> {
-    $graphql: NuxtGraphQLInstance;
+   $graphql: {
+      [client: string]: GraphQLClient;
+    };
   }
 }
